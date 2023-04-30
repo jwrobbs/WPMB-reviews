d85a83f9543e54499895d8afa090765c6ec818c6
Josh Robbs <josh@joshrobbs.com>
2023-04-30 10:51:25 -0400

    Add shortcode bridge function

--- php/review-shortcode.php
+++ php/review-shortcode.php
@@ -7,4 +7,19 @@
 
 namespace WPMB_Reviews\Review_Shortcode;
 
+use WPMB_Reviews\classes\Review_Functions;
+
 defined( 'ABSPATH' ) || exit;
+
+/**
+ * Shortcode to display review data
+ *
+ * Bridge function connecting the shortcode to the Review_Functions class.
+ *
+ * @param array $atts Shortcode attributes. Not used.
+ * @return string
+ */
+function wpmb_review_shortcode( $atts ) {
+	return Review_Functions::wpmb_review_html( get_the_ID() );
+}
+add_shortcode( 'wpmb-review-data', __NAMESPACE__ . '\wpmb_review_shortcode' );
